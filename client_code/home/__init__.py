a669d9523f12f0ad91e393064684179ff3e27b08
mattsantos541@gmail.com <mattsantos541@gmail.com>
2024-02-16 22:21:56 +0000

    Edited register and home

--- client_code/home/__init__.py
+++ client_code/home/__init__.py
@@ -7,3 +7,7 @@ class home(homeTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_register_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('register')
