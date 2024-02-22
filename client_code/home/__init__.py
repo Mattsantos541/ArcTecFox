d9819cee2ab54febee69d7f6bc3b704edb1a9a59
mattsantos541@gmail.com <mattsantos541@gmail.com>
2024-02-22 16:49:58 +0000

    Created register; Edited dash and home

--- client_code/home/__init__.py
+++ client_code/home/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import homeTemplate
 from anvil import *
+from ..register import register
 
 class home(homeTemplate):
   def __init__(self, **properties):
@@ -8,30 +9,8 @@ class home(homeTemplate):
 
     # Any code you write here will run before the form opens.
 
-  def button_register_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('register')
-
-  def link_dash_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
-
-  def link_dg_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
-
-  def link_scorecard_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
-
-  def link_login_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
-
-  def link_account_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
-
-  def link_logout_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
+  def link_home_click(self, **event_args):
+      cmpt= 
+     # Load Form1 into the ContentPanel
+      self.content_panel.clear()  # Clear existing content
+      self.content_panel.add_component(cmpt)
