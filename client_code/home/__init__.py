e0c0efcebe2dd0355a91fdb3ea1c4384e1f19d47
mattsantos541@gmail.com <mattsantos541@gmail.com>
2024-02-22 16:17:56 +0000

    Edited theme/parameters.yaml and home; Deleted register

--- client_code/home/__init__.py
+++ client_code/home/__init__.py
@@ -11,3 +11,27 @@ class home(homeTemplate):
   def button_register_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('register')
+
+  def link_dash_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_dg_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_scorecard_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_login_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_account_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_logout_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
