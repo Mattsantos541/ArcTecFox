133e91a87e1b453768924cef05b5087c0264f66b
mattsantos541@gmail.com <mattsantos541@gmail.com>
2024-02-22 22:23:50 +0000

    Edited home

--- client_code/home/__init__.py
+++ client_code/home/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import homeTemplate
 from anvil import *
+from ..vault import vault
 
 class home(homeTemplate):
   def __init__(self, **properties):
@@ -7,3 +8,9 @@ class home(homeTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_vault_click(self, **event_args):
+    cmpt = vault()
+
+    self.column_panel_content.clear()
+    self.column_panel_content.add_component(cmpt)
