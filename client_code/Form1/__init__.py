b2d95561384e51bed5feb0bd965978aa9b066792
mattsantos541@gmail.com <mattsantos541@gmail.com>
2024-02-22 22:09:23 +0000

    Created Form1; Edited settings; Deleted account, dash, datagen, home and 2 more

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,7 +1,7 @@
-from ._anvil_designer import registerTemplate
+from ._anvil_designer import Form1Template
 from anvil import *
 
-class register(registerTemplate):
+class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
