344e2f994165cc287fcf432d99689a2a9602cd71
mattsantos541@gmail.com <mattsantos541@gmail.com>
2024-02-23 21:14:52 +0000

    Edited home

--- client_code/home/__init__.py
+++ client_code/home/__init__.py
@@ -1,7 +1,7 @@
 from ._anvil_designer import homeTemplate
 from anvil import *
 from ..vault import vault
-
+from ..homeanon import homeanon
 class home(homeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -10,9 +10,33 @@ class home(homeTemplate):
     # Any code you write here will run before the form opens.
 
   def link_vault_click(self, **event_args):
-    cmpt = vault()
     self.link_vault.role = 'selected'
+    self.load_component(vault())
 
+
+  def link_home_click(self, **event_args):
+    self.load_component(homeanon())
+
+  def link_datagen_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_scorecard_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_account_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_signin_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_logout_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def load_component(self, cmpt):
     self.column_panel_content.clear()
     self.column_panel_content.add_component(cmpt)
-
