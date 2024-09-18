29a6d5729d3f1286b2d11a442b868caa2aca30d0
mattsantos541@gmail.com <mattsantos541@gmail.com>
2024-09-18 20:59:30 +0000

    Created datagen.RowTemplate8; Edited datagen

--- client_code/datagen/__init__.py
+++ client_code/datagen/__init__.py
@@ -11,8 +11,26 @@ class datagen(datagenTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    # Disable preview button initially
+    self.button_preview.enabled = False
+        
+    # Load datasets from the Vault
+    self.load_vault_datasets()
 
 
 
   #on generate button click, show loading bar and on completion go to scorecard page
+
+def load_vault_datasets(self, **event_args):
+    """Load datasets from the Vault and populate the dropdown."""
+    # Call server function to get the list of datasets
+    datasets = anvil.server.call('get_vault_datasets')
+    
+    # Populate the dropdown with dataset names and their IDs
+    self.dropdown_vault_datasets.items = [(row['dataset_name'], row['id']) for row in datasets]
+    
+    # Enable the dropdown if datasets are found
+    if datasets:
+        self.dropdown_vault_datasets.enabled = True
+    else:
+        self.dropdown_vault_datasets.enabled = False  # Disable dropdown if no datasets are found
